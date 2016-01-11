e8a6fbb7f0692df5c2396f16dc890d48f6ff86f4
Shahar Barsheshet <shaharbarsheshet@Shahars-MacBook-Pro-2.local>
2016-01-11 22:30:21 +0200

    init project

---
+++ KeyHash Generator/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  KeyHash Generator
+//
+//  Created by Shahar Barsheshet on 4/30/14.
+//  Copyright (c) 2014 PeleBit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
